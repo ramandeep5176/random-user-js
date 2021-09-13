20ab96079c98e28fa282e421fd4441a398f8f984
ramandeep5176 <ramandeepgill5176@gmail.com>
2021-09-13 18:23:23 -0400

    random-user-done

--- app.js
+++ app.js
@@ -2,27 +2,11 @@
 
 import get from "./utils/getElement.js";
 import getUser from "./utils/fetchuser.js";
+import displayUser from "./utils/dispayUser.js";
 const URL = "https://randomuser.me/api/";
 
-const img = get(".user-img");
-const title = get(".user-title");
-const value = get(".user-value");
 const btn = get(".btn");
-const btns = [...document.querySelectorAll(".icon")];
 
-const displayUser = (person) => {
-  img.src = person.image;
-  value.textContent = person.name;
-  title.textContent = `My name is `;
-  btns[0].classList.add("active");
-  btns.forEach((btn) => {
-    const label = btn.dataset.label;
-    btn.addEventListener("click", () => {
-      title.textContent = `my ${label} is`;
-      value.textContent = person[label];
-    });
-  });
-};
 const showUser = async () => {
   //
   const person = await getUser();
