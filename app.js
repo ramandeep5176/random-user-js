e80bb903c8ff0d63579cc2f841f4f10f2314f895
ramandeep5176 <ramandeepgill5176@gmail.com>
2022-01-20 17:19:33 -0500

    Displayed user

--- app.js
+++ app.js
@@ -6,42 +6,58 @@ const btn = getElement(".btn");
 const title = getElement(".user-title");
 const value = getElement(".user-value");
 const btns = [...document.querySelectorAll(".icon")];
-console.log(btns);
 // ruf
 
-function getUser(URL) {
-  btn.addEventListener("click", async () => {
-    try {
-      const data = await fetch(URL);
-      const response = await data.json();
-      // itterate object and get single user
-      const singleUser = response.results[0];
-      // console.log(singleUser);
-      const { gender, email, phone, cell, nat } = singleUser;
-      const { title, first, last } = singleUser.name;
-      console.log(gender, email, phone, cell, nat, title, first, last);
-      return {
-        gender,
-        email,
-        phone,
-        cell,
-        nat,
-        title,
-        first,
-        last,
-      };
-    } catch (error) {
-      console.log(error);
-    }
-  });
-}
-getUser(URL);
+const getUser = async () => {
+  const data = await fetch(URL);
+  const response = await data.json();
+  // console.log(response);
+  // itterate object and get single user
+  const singleUser = response.results[0];
+  // console.log(singleUser);
+  const { phone, email } = singleUser;
+  const { medium: image } = singleUser.picture;
+  const { password } = singleUser.login;
+  const { first, last } = singleUser.name;
+  const {
+    dob: { age },
+  } = singleUser;
+  const {
+    street: { number, name },
+  } = singleUser.location;
+  return {
+    phone,
+    email,
+    image,
+    password,
+    age,
+    street: `${number} ${name}`,
+    name: `${first} ${last}`,
+  };
+  // console.log(gender, email, phone, cell, nat, title, first, last, medium);
+};
+
+const display = (singleUser) => {
+  img.src = singleUser.image;
+  title.textContent = `my name is  `;
+  value.textContent = singleUser.name;
+// all btns
+btns[0].classList.add('active')
+
+
+// all btns end
+
+
+};
 
 // show user
-function show() {}
-console.log(show);
+const show = async () => {
+  const singleUser = await getUser();
+  display(singleUser);
+};
 // show user end
 
-window.addEventListener("DOMContentLoaded", show());
+btn.addEventListener("click", show);
+window.addEventListener("DOMContentLoaded", show);
 
 // new
